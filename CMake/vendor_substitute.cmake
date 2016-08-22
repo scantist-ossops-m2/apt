e986505c5745b92ef1a1c64af5f236a6f11ba227
Julian Andres Klode <jak@debian.org>
2016-08-22 23:57:27 +0200

    CMake: vendor_substitute: Only run getinfo for existing vars
    If the variable to be replaced does not exist in the string, there
    is no point in invoking getinfo.

--- CMake/vendor_substitute.cmake
+++ CMake/vendor_substitute.cmake
@@ -1,8 +1,10 @@
 file(READ ${IN} input)
 foreach(variable ${VARS})
-    execute_process(COMMAND ${PROJECT_SOURCE_DIR}/vendor/getinfo
-                            --vendor ${CURRENT_VENDOR} ${variable}
-                    OUTPUT_VARIABLE value OUTPUT_STRIP_TRAILING_WHITESPACE)
-    string(REPLACE "&${variable};" "${value}" input "${input}")
+    if (input MATCHES "${variable}")
+        execute_process(COMMAND ${PROJECT_SOURCE_DIR}/vendor/getinfo
+                                --vendor ${CURRENT_VENDOR} ${variable}
+                        OUTPUT_VARIABLE value OUTPUT_STRIP_TRAILING_WHITESPACE)
+        string(REPLACE "&${variable};" "${value}" input "${input}")
+    endif()
 endforeach()
 file(WRITE ${OUT} "${input}")
