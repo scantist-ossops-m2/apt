scantist-ossops-m2/apt

commit 8f858d560e3b7b475c623c4e242d1edce246025a

don't leak FD in AutoProxyDetect command return parsing
Just closing the fd would be enough, but while we are at it we can also
use the Popen interface to have an easier time with this.

apt-pkg/contrib/proxy.cc
@@ -16,9 +16,9 @@
 #include <unistd.h>
 
 #include "proxy.h"
+									/*}}}*/
 
-
-// AutoDetectProxy - auto detect proxy          			/*{{{*/
+// AutoDetectProxy - auto detect proxy					/*{{{*/
 // ---------------------------------------------------------------------
 /* */
 bool AutoDetectProxy(URI &URL)
@@ -41,45 +41,30 @@ bool AutoDetectProxy(URI &URL)
    if (Debug)
       std::clog << "Using auto proxy detect command: " << AutoDetectProxyCmd << std::endl;
 
-   int Pipes[2] = {-1,-1};
-   if (pipe(Pipes) != 0)
-      return _error->Errno("pipe", "Failed to create Pipe");
-
-   pid_t Process = ExecFork();
-   if (Process == 0)
-   {
-      close(Pipes[0]);
-      dup2(Pipes[1],STDOUT_FILENO);
-      SetCloseExec(STDOUT_FILENO,false);
-
-      std::string foo = URL;
-      const char *Args[4];
-      Args[0] = AutoDetectProxyCmd.c_str();
-      Args[1] = foo.c_str();
-      Args[2] = 0;
-      execv(Args[0],(char **)Args);
-      std::cerr << "Failed to exec method " << Args[0] << std::endl;
-      _exit(100);
-   }
+   std::string const urlstring = URL;
+   std::vector<const char *> Args;
+   Args.push_back(AutoDetectProxyCmd.c_str());
+   Args.push_back(urlstring.c_str());
+   Args.push_back(nullptr);
+   FileFd PipeFd;
+   pid_t Child;
+   if(Popen(&Args[0], PipeFd, Child, FileFd::ReadOnly) == false)
+      return _error->Error("ProxyAutoDetect command '%s' failed!", AutoDetectProxyCmd.c_str());
    char buf[512];
-   int InFd = Pipes[0];
-   close(Pipes[1]);
-   int res = read(InFd, buf, sizeof(buf)-1);
-   ExecWait(Process, "ProxyAutoDetect", true);
+   if (PipeFd.ReadLine(buf, sizeof(buf)) == nullptr)
+      return _error->Error("Failed to read in AutoDetectProxy");
+   PipeFd.Close();
+   ExecWait(Child, "ProxyAutoDetect", true);
+   auto const cleanedbuf = _strstrip(buf);
 
-   if (res < 0)
-      return _error->Errno("read", "Failed to read");
-   if (res == 0)
+   if (cleanedbuf[0] == '\0')
       return _error->Warning("ProxyAutoDetect returned no data");
 
-   // add trailing \0
-   buf[res] = 0;
-
    if (Debug)
-      std::clog << "auto detect command returned: '" << buf << "'" << std::endl;
+      std::clog << "auto detect command returned: '" << cleanedbuf << "'" << std::endl;
 
-   if (strstr(buf, URL.Access.c_str()) == buf)
-      _config->Set("Acquire::"+URL.Access+"::proxy::"+URL.Host, _strstrip(buf));
+   if (strstr(cleanedbuf, URL.Access.c_str()) == cleanedbuf)
+      _config->Set("Acquire::"+URL.Access+"::proxy::"+URL.Host, cleanedbuf);
 
    return true;
 }

commit a789a9dfdab4898c343f34d9ded83574b85d6d44

Do not read stderr from proxy autodetection scripts
This fixes a regression introduced in
  commit 8f858d560e3b7b475c623c4e242d1edce246025a

  don't leak FD in AutoProxyDetect command return parsing

which accidentally made the proxy autodetection code also read
the scripts output on stderr, not only on stdout when it switched
the code from popen() to Popen().

Reported-By: Tim Small <tim@seoss.co.uk>

apt-pkg/contrib/fileutl.cc
@@ -2861,6 +2861,11 @@ bool Rename(std::string From, std::string To)				/*{{{*/
 									/*}}}*/
 bool Popen(const char* Args[], FileFd &Fd, pid_t &Child, FileFd::OpenMode Mode)/*{{{*/
 {
+   return Popen(Args, Fd, Child, Mode, true);
+}
+									/*}}}*/
+bool Popen(const char* Args[], FileFd &Fd, pid_t &Child, FileFd::OpenMode Mode, bool CaptureStderr)/*{{{*/
+{
    int fd;
    if (Mode != FileFd::ReadOnly && Mode != FileFd::WriteOnly)
       return _error->Error("Popen supports ReadOnly (x)or WriteOnly mode only");
@@ -2891,7 +2896,8 @@ bool Popen(const char* Args[], FileFd &Fd, pid_t &Child, FileFd::OpenMode Mode)/
       if(Mode == FileFd::ReadOnly)
       {
          dup2(fd, 1);
-         dup2(fd, 2);
+	 if (CaptureStderr == true)
+	    dup2(fd, 2);
       } else if(Mode == FileFd::WriteOnly)
          dup2(fd, 0);
 

apt-pkg/contrib/fileutl.h
@@ -248,8 +248,11 @@ std::vector<std::string> Glob(std::string const &pattern, int flags=0);
  * \param Child a reference to the integer that stores the child pid
  *        Note that you must call ExecWait() or similar to cleanup
  * \param Mode is either FileFd::ReadOnly or FileFd::WriteOnly
+ * \param CaptureStderr True if we should capture stderr in addition to stdout.
+ *                      (default: True).
  * \return true on success, false on failure with _error set
  */
+bool Popen(const char* Args[], FileFd &Fd, pid_t &Child, FileFd::OpenMode Mode, bool CaptureStderr);
 bool Popen(const char* Args[], FileFd &Fd, pid_t &Child, FileFd::OpenMode Mode);
 
 

apt-pkg/contrib/proxy.cc
@@ -48,7 +48,7 @@ bool AutoDetectProxy(URI &URL)
    Args.push_back(nullptr);
    FileFd PipeFd;
    pid_t Child;
-   if(Popen(&Args[0], PipeFd, Child, FileFd::ReadOnly) == false)
+   if(Popen(&Args[0], PipeFd, Child, FileFd::ReadOnly, false) == false)
       return _error->Error("ProxyAutoDetect command '%s' failed!", AutoDetectProxyCmd.c_str());
    char buf[512];
    bool const goodread = PipeFd.ReadLine(buf, sizeof(buf)) != nullptr;

test/libapt/apt-proxy-script
@@ -1,0 +1,9 @@
+#!/bin/sh
+
+if [ $1 = "http://www.debian.org:90/temp/test" ]; then
+    echo "http://example.com"
+fi
+if [ $1 = "http://www.debian.org:91/temp/test" ]; then
+    echo "This works" >&2
+    echo "http://example.com/foo"
+fi

test/libapt/uri_test.cc
@@ -1,4 +1,6 @@
 #include <config.h>
+#include <apt-pkg/configuration.h>
+#include <apt-pkg/proxy.h>
 #include <apt-pkg/strutl.h>
 #include <string>
 #include <gtest/gtest.h>
@@ -187,4 +189,28 @@ TEST(URITest, RFC2732)
    EXPECT_EQ("ftp://example.org", URI::SiteOnly(U));
    EXPECT_EQ("ftp://example.org", URI::ArchiveOnly(U));
    EXPECT_EQ("ftp://example.org/", URI::NoUserPassword(U));
+}
+TEST(URITest, AutoProxyTest)
+{
+   URI u0("http://www.debian.org:90/temp/test");
+   URI u1("http://www.debian.org:91/temp/test");
+
+   _config->Set("Acquire::http::Proxy-Auto-Detect", "./apt-proxy-script");
+
+   // Scenario 0: Autodetecting a simple proxy
+   AutoDetectProxy(u0);
+   EXPECT_EQ(_config->Find("Acquire::http::proxy::www.debian.org", ""), "http://example.com");
+
+   // Scenario 1: Proxy stays the same if it is already set
+   AutoDetectProxy(u1);
+   EXPECT_EQ(_config->Find("Acquire::http::proxy::www.debian.org", ""), "http://example.com");
+
+   // Scenario 2: Reading with stderr output works fine
+   _config->Clear("Acquire::http::proxy::www.debian.org");
+   AutoDetectProxy(u1);
+   EXPECT_EQ(_config->Find("Acquire::http::proxy::www.debian.org", ""), "http://example.com/foo");
+
+   // Scenario 1 again: Proxy stays the same if it is already set
+   AutoDetectProxy(u0);
+   EXPECT_EQ(_config->Find("Acquire::http::proxy::www.debian.org", ""), "http://example.com/foo");
 }