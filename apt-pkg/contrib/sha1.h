047f0a99139d267417884ba3ad1428d365916bda
Julian Andres Klode <julian.klode@canonical.com>
2020-01-07 17:33:50 +0100

    Convert SHA1Summation to gcrypt

--- apt-pkg/contrib/sha1.h
+++ apt-pkg/contrib/sha1.h
@@ -18,20 +18,9 @@
 
 typedef  HashSumValue<160> SHA1SumValue;
 
-class SHA1Summation : public SummationImplementation
+struct SHA1Summation : public SummationImplementation
 {
-   /* assumes 64-bit alignment just in case */
-   unsigned char Buffer[64] __attribute__((aligned(8)));
-   unsigned char State[5*4] __attribute__((aligned(8)));
-   unsigned char Count[2*4] __attribute__((aligned(8)));
-   bool Done;
-   
-   public:
-   bool Add(const unsigned char *inbuf, unsigned long long inlen) APT_OVERRIDE APT_NONNULL(2);
-   using SummationImplementation::Add;
-
    SHA1SumValue Result();
-   
    SHA1Summation();
 };
 
